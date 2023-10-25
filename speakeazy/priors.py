98a2f8987a1af0c1234144851fb6565c0d5f9053
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-10-25 21:18:41 +0200

    add init log prior in prior file

--- speakeazy/priors.py
+++ speakeazy/priors.py
@@ -28,6 +28,7 @@ class Priors(object):
         self.lw = lw
         self.lr = lr
         self.set_params(fix_ns,nspline,epoly,ppoly,vel_width,vel_width_broad,scale_disp,z,z0,halpha_prism,scale_p,broadlines)    
+        self.init_logprior()
         
     def set_params(self,fix_ns=True,nspline=13,epoly=3,ppoly=3,vel_width=100.,vel_width_broad=300.,scale_disp=1.3,z=None,z0=[1.4,1.5],halpha_prism='free',scale_p=False,broadlines=False):
         
