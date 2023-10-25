92ff69537db07b3c5277fa94e6793767e31313ac
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-10-25 19:29:51 +0200

    fix test error

--- speakeazy/tests/test_sampling.py
+++ speakeazy/tests/test_sampling.py
@@ -6,8 +6,9 @@ from .. import data,priors,sampling
 
 class TestSampler():
     def setup(self):
-         _spec_file = os.path.join(os.path.dirname(__file__), 
-                                          'data/macs0417.1208_340.v0.spec.fits')
+         spec_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
+         _spec_file = os.path.join(spec_dir, 
+                                          'macs0417.1208_340.v0.spec.fits')
          spectrum = data.Data(spectrum_file=_spec_file,photometry_file=None,run_ID=1,phot_id=None)
          prs = priors.Priors(spectrum)
          self.sampler = sampling.Sampler(spectrum,prs)
