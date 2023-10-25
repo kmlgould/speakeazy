a236b96ec93478124e5d61ac9677514598d59203
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-10-25 19:15:12 +0200

    fix test error

--- speakeazy/tests/test_sampling.py
+++ speakeazy/tests/test_sampling.py
@@ -10,41 +10,41 @@ class TestSampler():
          self.sampler = sampling.Sampler(spectrum,prs)
 
 
-def test_prior_sampling(self):
-    """test prior sampling for setting up walkers
-    
-    - `speakeazy.sampling.Sampler.make_norm_prior`
-    
-    - `speakeazy.sampling.Sampler.sample_from_priors`
-    """
-    
-    # test generation of gaussian priors 
-    
-    from scipy.stats import norm 
-    
-    norm_prior_test = norm(loc=0.,scale=1.)
-    norm_prior_sample = norm_prior_test.rvs(size=100)
-    
-    norm_prior = self.sampler.make_norm_prior(sampling=True)
-    
-    assert np.allclose(norm_prior_sample,norm_prior,tol=1e-3)
+    def test_prior_sampling(self):
+        """test prior sampling for setting up walkers
+        
+        - `speakeazy.sampling.Sampler.make_norm_prior`
+        
+        - `speakeazy.sampling.Sampler.sample_from_priors`
+        """
+        
+        # test generation of gaussian priors 
+        
+        from scipy.stats import norm 
+        
+        norm_prior_test = norm(loc=0.,scale=1.)
+        norm_prior_sample = norm_prior_test.rvs(size=100)
+        
+        norm_prior = self.sampler.make_norm_prior(sampling=True)
+        
+        assert np.allclose(norm_prior_sample,norm_prior,tol=1e-3)
 
-    # test generation of walkers based on priors - FINISH THIS 
-    #nwalkers = 100
-    #nparam = 1
-    #test_prior_matrix = np.zeros([nwalkers,nparam])
-    
-    #real_prior_matrix = self.sampler.sample_from_priors()
-    
-    #assert np.allclose(real_prior_matrix,test_prior_matrix,tol=1e-3)
-    
-    #return test_prior_matrix
-    
-    
+        # test generation of walkers based on priors - FINISH THIS 
+        #nwalkers = 100
+        #nparam = 1
+        #test_prior_matrix = np.zeros([nwalkers,nparam])
+        
+        #real_prior_matrix = self.sampler.sample_from_priors()
+        
+        #assert np.allclose(real_prior_matrix,test_prior_matrix,tol=1e-3)
+        
+        #return test_prior_matrix
+        
+        
 
-    
-#def test_walker_init():
-#     """test initialisation of parameter walkers for sampling with Emcee
-#    
-#    - `speakeazy.sampling.Sampler.init_walkers`
-#    """   
+        
+    #def test_walker_init():
+    #     """test initialisation of parameter walkers for sampling with Emcee
+    #    
+    #    - `speakeazy.sampling.Sampler.init_walkers`
+    #    """   
