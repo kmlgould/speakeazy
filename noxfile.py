ed351f88b1bd91209f4c89f677b96cfcfe753ee6
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-10-25 16:49:46 +0200

    noxfile

---
+++ noxfile.py
@@ -0,0 +1,13 @@
+import nox
+
+@nox.session(python="3.8")
+def tests(session):
+    session.install(".")
+    session.install("pytest")
+    
+    if session.posargs:
+        test_files = session.posargs
+    else:
+        test_files = ['speakeazy/tests/test_sampling.py']
+
+    session.run('pytest', *test_files)
