bcf765f6c6920de5cd2901db75527b8ffd9486bd
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-11-04 16:37:54 +0100

    added option to see individual gaussian lines in plot

--- speakeazy/tests/test_fitting.py
+++ speakeazy/tests/test_fitting.py
@@ -9,9 +9,12 @@ class TestSampler():
          spec_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
          _spec_file = os.path.join(spec_dir, 
                                           'macs0417.1208_340.v0.spec.fits')
-         spectrum = data.Data(spectrum_file=_spec_file,photometry_file=None,run_ID=1,phot_id=None)
-         prs = priors.Priors(spectrum)
+         self.spectrum = data.Data(spectrum_file=_spec_file,photometry_file=None,run_ID=1,phot_id=None)
+         self.priors = priors.Priors(self.spectrum)
          
-         fit_object = fitting.Fitter(spectrum,prs)
+
+def test_fitting(self):
+         
+         fit_object = fitting.Fitter(self.spectrum,self.priors)
          fit_object.fit_redshift_chisq()
          fit_object.plot_spectrum()
