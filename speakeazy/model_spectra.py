49361b0fa02894c3fc4da2d580099f08ab2cf674
kmlgould <71020832+kmlgould@users.noreply.github.com>
2023-11-04 23:41:35 +0100

    changed to bounded lst squares solver

--- speakeazy/model_spectra.py
+++ speakeazy/model_spectra.py
@@ -246,12 +246,14 @@ class Model(object):
         lb = np.zeros(NTEMP)
         
         # emission lines are lower bounded at -inf 
-        lb[:self.nlines] = -np.inf
+        lb[:self.nlines] = -1e9
         # broad lines and continuum must be lower bounded at zero 
         lb[self.nlines:] = 0.
         
         # upper bounds for everything can be up to inf basically 
-        ub = np.ones(NTEMP)*np.inf
+        ub = np.ones(NTEMP)*1e9
+        
+        
 
         self.fit_bounds = (lb,ub)
         
