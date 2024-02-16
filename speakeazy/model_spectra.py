12e303bf114b22c180f5ac3d56d43e9b4c271f65
kmlgould <71020832+kmlgould@users.noreply.github.com>
2024-02-16 14:09:26 +0100

    abs line bug

--- speakeazy/model_spectra.py
+++ speakeazy/model_spectra.py
@@ -279,8 +279,8 @@ class Model(object):
             # emission lines are lower bounded at -inf 
             lb[:self.nlines] = -1000.
             # halpha and paschen lines must be positive 
-            lb[:self.nlines][lmask] = 1e-1 
-            lb[:self.nlines][~lmask] = -1000.
+            #lb[:self.nlines][lmask] = 1e-1 
+            #lb[:self.nlines][~lmask] = -1000.
             # broad lines and continuum must be lower bounded at zero 
             lb[self.nlines:] = 1e-6
             
