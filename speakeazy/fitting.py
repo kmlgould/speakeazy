ec639305145811a2b2ad1996d8a4f7f5c2ca231e
kmlgould <71020832+kmlgould@users.noreply.github.com>
2024-02-16 14:12:22 +0100

    abs line bug

--- speakeazy/fitting.py
+++ speakeazy/fitting.py
@@ -131,7 +131,7 @@ class Fitter(object):
             else:
                 fit_bounds = self.model.fit_bounds
                 masked_fit_bounds = (fit_bounds[0][:][okt] ,fit_bounds[1][:][okt])
-                #print(masked_fit_bounds)
+                print(masked_fit_bounds)
                 res = lsq_linear(_Ax[:,mask].T, 
                                      _yx[mask],
                                      bounds=masked_fit_bounds,tol=1e-10,verbose=False)
